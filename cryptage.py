90b4d205aadd83e878562b3e8276c533c93f4ee6
TheAngelLCF <65764028+TheAngelLCF@users.noreply.github.com>
2022-04-07 15:37:36 +0200

    first commit
    Initilisation des fichiers sur le serveur de GitHub

---
+++ cryptage.py
@@ -0,0 +1,116 @@
+# Code LuxFerre | Cryptage
+import os
+
+def compile_str(string: str) -> list:
+    final = []
+    for elt in string:
+        if(elt == " "): final.append(200)
+        else: final.append(ord(elt))
+    return final
+
+def adding_decal(liste: list, nbr: int) -> list:
+    final = []
+    for elt in liste:
+        if(elt + nbr < 100): raise ValueError("Merci de mettre un nombre qui permet d'avoir plus de 100 partout !")
+        final.append(elt + nbr)
+    return final
+
+def crypte():
+    phrase = input("Merci de mettre votre phrase ici: ")
+    compiled = compile_str(phrase)
+    minn = 100 - min(compiled)
+    while True:
+        try:
+            nbr = int(input("Combien voulez-vous de décalage (minimum " + str(minn) + "): "))
+            if(nbr < minn): print("Meric de mettre un nombre plus grand que", minn)
+            else: break
+        except: pass
+    
+    final = adding_decal(compiled, nbr)
+    liste_code = [oct, hex]
+    from random import shuffle
+    shuffle(liste_code)
+    
+    string = ''
+    for elt in final:
+        string += str(elt)
+    
+    string = str(liste_code[0](int(string)))[2:]
+    print("Base utilisé", str(liste_code[0]))
+    print("Voici le cryptage:")
+        
+    return string
+
+def read_file(file_name: str, directory='./') -> list:
+    with open(directory + file_name, 'r', encoding='UTF-8') as file:
+        liste = file.readlines()
+    
+    final = []
+    for k in range(len(liste) - 1):
+        final.append(liste[k][:-1])
+    final.append(liste[-1])
+    
+    return final
+
+def write_file(file_name: str, to_write: list, directory='./'):
+    import os
+    if(file_name in os.listdir(directory)): os.remove(directory + file_name)
+    
+    with open(directory + file_name, 'a', encoding='UTF-8') as file:
+        for k in range(len(to_write) - 1):
+            file.write(to_write[k] + '\n')
+        file.write(to_write[-1])
+        
+    return True
+
+def crypte_file():
+    file_name = input("Merci de spécifier le nom du fichier: ")
+    
+    compiled = []
+    for elt in read_file(file_name):
+        compiled.append(compile_str(elt))
+        
+    # Recherche du min
+    
+    minn = min(compiled[0])
+    for k in range(1, len(compiled)):
+        if(minn > min(compiled[k])):
+            minn = min(compiled[k])
+           
+    
+    while True:
+        try:
+            nbr = int(input("Combien voulez-vous de décalage (minimum " + str(minn) + "): "))
+            if(nbr < minn): print("Meric de mettre un nombre plus grand que", minn)
+            else: break
+        except: pass
+        
+    final = []
+    for elt in compiled:
+        final.append(adding_decal(elt, nbr))
+        
+    final_string = ''
+    
+    for liste in final:
+        temps = ''
+        for value in liste:
+            temps += str(value)
+        temps = str(hex(int(temps)))[2:]
+        final_string += temps + '\n'
+        
+    final_string = final_string[:-1]
+    
+    if(file_name + '.crypte' in os.listdir()):
+        choix = ''
+        while choix != 'oui' and choix != 'non':
+            choix = input("Fichier de sortie déjà créer, voulez-vous le supprimer ? ").lower()
+            
+        if(choix == 'oui'): os.remove(file_name + '.crypte')
+        else:
+            print("Impossible d'enregistrer le fichier !")
+            return final_string
+    
+    with open(file_name + '.crypte', 'a', encoding='UTF-8') as file:
+        file.write(final_string)
+        
+    print('Fichier de sortie: ' + file_name + '.crypte')
---
+++ decodage.py
@@ -0,0 +1,98 @@
+# Code LuxFerre | Decodage
+import os
+
+def separation(string: str) -> list:
+    assert len(string) % 3 == 0, "Le code que vous avez donner n'est psa compatible avec le cryptage LuxFerre !"
+    temps = ''
+    final = []
+    for rang in range(len(string)):
+        temps += string[rang]
+        if((rang + 1) % 3 == 0 and rang != 0):
+            final.append(int(temps))
+            temps = ''
+    return final
+
+def delete_crypte(liste: list, nbr: int) -> list:
+    final = []
+    for elt in liste:
+        final.append(elt - nbr)
+    return final
+
+def decompile(liste: list) -> str:
+    string = ''
+    for elt in liste:
+        if(elt == 200): string += " "
+        else: string += chr(elt)
+    return string
+
+def formate(string: str, base: int):
+    final = int(string, base)
+    return str(final)
+
+def decode():
+    code = input("Merci d'entrer le code a decodé: ")
+    while True:
+        try:
+            base = int(input("Merci d'entrer la base: "))
+            break
+        except: pass
+    
+    while True:
+        try:
+            decal = int(input("Merci d'entrer le code: "))
+            break
+        except: pass
+        
+    separe = separation(formate(code, base))
+    final = delete_crypte(separe, decal)
+    decompiled = decompile(final)
+    print("Voici ce que nous avons décodé:")
+    print(decompiled)
+    
+def read_file(file_name: str):
+    with open(file_name, 'r', encoding='UTF-8') as file:
+        read = file.readlines()
+    
+    final = []
+    for elt in read:
+        if('\n' in elt): final.append(elt[:-1])
+        else: final.append(elt)
+        
+    return final
+
+def decode_file():
+    file_name = input("Merci d'entrer le code a decodé: ")
+    base = 16
+    while True:
+        try:
+            decal = int(input("Merci d'entrer le code: "))
+            break
+        except: pass
+        
+    liste_base = read_file(file_name)
+    
+    final = []
+    for ligne in liste_base:
+        final.append(separation(formate(ligne, base)))
+    
+    uncrypted = []
+    for elt in final:
+        uncrypted.append(delete_crypte(elt, decal))
+        
+    decompiled = []
+    for elt in uncrypted:
+        decompiled.append(decompile(elt))
+        
+    final_string = ''
+    for elt in decompiled:
+        final_string += elt + '\n'
+    final_string = final_string[:-1]
+    
+    fichier_sorti = input("Merci de préciser le fichier de sortie ici (attention, si un fichier existe alors il sera remplacé): ")
+    
+    if(fichier_sorti in os.listdir()): os.remove(fichier_sortie)
+    
+    with open(fichier_sorti, 'a', encoding='UTF-8') as file:
+        file.write(final_string)
+        
+    print("Décryptage effectué !\nFichier généré avec succés !")
